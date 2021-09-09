de07658bb3ddea661e2ee81a315af3d1dcda862d
Mingxun Wang <mwang87@gmail.com>
2021-09-09 11:06:45 -0700

    fixing linkouts

--- workflow/bin/reformat.py
+++ workflow/bin/reformat.py
@@ -8,15 +8,15 @@ input_folder = sys.argv[1]
 
 # Final Results
 input_final_results_filename = os.path.join(input_folder, "magi_results.csv")
-results_df = pd.read_csv(input_final_results_filename, sep=",")
+results_df = pd.read_csv(input_final_results_filename, sep=",", dtype=str)
 results_df.to_csv("magi_results.tsv", sep="\t", index=False)
 
 # Compound Results
 input_final_results_filename = os.path.join(input_folder, "magi_compound_results.csv")
-results_df = pd.read_csv(input_final_results_filename, sep=",")
+results_df = pd.read_csv(input_final_results_filename, sep=",", dtype=str)
 results_df.to_csv("magi_compound_results.tsv", sep="\t", index=False)
 
 # Gene Results
 input_final_results_filename = os.path.join(input_folder, "magi_gene_results.csv")
-results_df = pd.read_csv(input_final_results_filename, sep=",")
+results_df = pd.read_csv(input_final_results_filename, sep=",", dtype=str)
 results_df.to_csv("magi_gene_results.tsv", sep="\t", index=False)
