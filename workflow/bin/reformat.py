de97805d150654cf68e707abd5c63956067a8824
Mingxun Wang <mwang87@gmail.com>
2021-09-08 17:19:03 -0700

    updating reformat

--- workflow/bin/reformat.py
+++ workflow/bin/reformat.py
@@ -7,16 +7,16 @@ import pandas as pd
 input_folder = sys.argv[1]
 
 # Final Results
-input_final_results_filename = glob.glob(os.path.join(input_folder, "**", "magi_results.csv"), recursive=True)
+input_final_results_filename = os.path.join(input_folder, "magi_results.csv")
 results_df = pd.read_csv(input_final_results_filename, sep=",")
 results_df.to_csv("magi_results.tsv", sep="\t", index=False)
 
 # Compound Results
-input_final_results_filename = glob.glob(os.path.join(input_folder, "**", "magi_compound_results.csv"), recursive=True)
+input_final_results_filename = os.path.join(input_folder, "magi_compound_results.csv")
 results_df = pd.read_csv(input_final_results_filename, sep=",")
 results_df.to_csv("magi_compound_results.tsv", sep="\t", index=False)
 
 # Gene Results
-input_final_results_filename = glob.glob(os.path.join(input_folder, "**", "magi_gene_results.csv"), recursive=True)
+input_final_results_filename = os.path.join(input_folder, "magi_gene_results.csv")
 results_df = pd.read_csv(input_final_results_filename, sep=",")
 results_df.to_csv("magi_gene_results.tsv", sep="\t", index=False)
